52cbd397c7581f80eb97c5e29e84b8b72dc8f86c
Edgar <edgarvlzm26@gmail.com>
2023-10-29 22:26:51 -0600

    Data display screen update

--- CriticalSectionWindow.java
+++ CriticalSectionWindow.java
@@ -1,7 +1,10 @@
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class CriticalSectionWindow{
-    private int WIDTH = 800;
+    private int WIDTH = 700;
     private int HEIGTH = 600;
     private JFrame csWindowFrame;
 
@@ -9,6 +12,23 @@ public class CriticalSectionWindow{
         csWindowFrame = new JFrame(name);
         csWindowFrame.setSize(WIDTH, HEIGTH);
         csWindowFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+    }
+
+    public void createAndShowGUI(int threadNumber, int criticalTime){
+        
+        String[] columnNames = {"Name", "Accesses", "Waiting", "Critical Section", "Dying", "Dead"};
+        Object[][] threadData = new Object[threadNumber][];
+
+        for (int i = 0; i < threadNumber; i++) {
+            threadData[i] = new Object[]{"Thread "+ i,0};
+        }
+
+        JTable table = new JTable(new DefaultTableModel(threadData, columnNames));
+
+
+        JScrollPane scrollPane = new JScrollPane(table);
+        csWindowFrame.add(scrollPane);
         csWindowFrame.setVisible(true);
     }
 }
--- Window.java
+++ Window.java
@@ -1,16 +1,15 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
+import java.awt.Color;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 
 public class Window extends JFrame{
-    private int WIDTH = 400;
+    private int WIDTH = 500;
     private int HEIGTH = 150;
     private JLabel threadNumberLabel = new JLabel("Number of Threads:");
     private JLabel criticalTimeLabel = new JLabel("Time inside CS (ms):");
@@ -24,11 +23,14 @@ public class Window extends JFrame{
         setLayout(new GridLayout(3,2));
         setResizable(false);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        getContentPane().setBackground(new Color(40,40,40));
     }
 
     public void createAndShowGUI(){
         threadNumberLabel.setHorizontalAlignment(SwingConstants.CENTER);
         criticalTimeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        threadNumberLabel.setForeground(new Color(255,255,255));
+        criticalTimeLabel.setForeground(new Color(255,255,255));
 
         getContentPane().add(threadNumberLabel);
         getContentPane().add(threadNumberField);
@@ -36,10 +38,13 @@ public class Window extends JFrame{
         getContentPane().add(criticalTimeField);
 
         executeButton.addActionListener(e -> {
-            new CriticalSectionWindow("Data Display Screen");
+            CriticalSectionWindow CSWindow = new CriticalSectionWindow("Data Display Screen");
+            CSWindow.createAndShowGUI(Integer.parseInt(threadNumberField.getText()),10);
         });
         
         getContentPane().add(executeButton);
         setVisible(true);
     }
+
+    
 }
