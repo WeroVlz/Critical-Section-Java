63bc28d5b765c46a99d3b8678f12ea0b0fb228b5
Edgar <edgarvlzm26@gmail.com>
2023-10-31 08:18:09 -0600

    Validation now working

--- Window.java
+++ Window.java
@@ -1,6 +1,7 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -8,9 +9,7 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 
-
-
-public class Window extends JFrame{
+public class Window extends JFrame {
     private int WIDTH = 600;
     private int HEIGTH = 300;
     private JLabel threadNumberLabel = new JLabel("Number of Threads:");
@@ -23,7 +22,7 @@ public class Window extends JFrame{
     private JLabel courseName = new JLabel("Fundamentos de Programacion en Paralelo");
     private JLabel teacherName = new JLabel("Dr. Juan Carlos Lopez Pimentel");
 
-    public Window(String name){
+    public Window(String name) {
         super(name);
         setSize(WIDTH, HEIGTH);
         setLayout(new BorderLayout());
@@ -31,8 +30,8 @@ public class Window extends JFrame{
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
 
-    public void createAndShowGUI(){
-        
+    public void createAndShowGUI() {
+
         threadNumberLabel.setHorizontalAlignment(SwingConstants.CENTER);
         criticalTimeLabel.setHorizontalAlignment(SwingConstants.CENTER);
         studentEdgar.setHorizontalAlignment(SwingConstants.CENTER);
@@ -40,28 +39,39 @@ public class Window extends JFrame{
         courseName.setHorizontalAlignment(SwingConstants.CENTER);
         teacherName.setHorizontalAlignment(SwingConstants.CENTER);
 
-        JPanel inputPanel = new JPanel(new GridLayout(2,2));
+        JPanel inputPanel = new JPanel(new GridLayout(2, 2));
         inputPanel.add(threadNumberLabel);
         inputPanel.add(threadNumberField);
         inputPanel.add(criticalTimeLabel);
         inputPanel.add(criticalTimeField);
-        inputPanel.setPreferredSize(new Dimension(600,100));
+        inputPanel.setPreferredSize(new Dimension(600, 100));
         getContentPane().add(inputPanel, BorderLayout.NORTH);
 
         executeButton.addActionListener(e -> {
-            CriticalSectionWindow CSWindow = new CriticalSectionWindow("Data Display Screen");
-            CSWindow.createAndShowGUI(Integer.parseInt(threadNumberField.getText()),
-                                      Integer.parseInt(criticalTimeField.getText()));
+            String threadNumber = threadNumberField.getText();
+            String criticalTime = criticalTimeField.getText();
+
+            if (threadNumber.isEmpty() || criticalTime.isEmpty()) {
+                JOptionPane.showMessageDialog(this, "Please don't leave empty fields.");
+            } else {
+                try {
+                    CriticalSectionWindow CSWindow = new CriticalSectionWindow("Data Display Screen");
+                    CSWindow.createAndShowGUI(Integer.parseInt(threadNumber), Integer.parseInt(criticalTime));
+                } catch (NumberFormatException err) {
+                    JOptionPane.showMessageDialog(this, "Please only enter numeric values.");
+                }
+            }
+
         });
 
         getContentPane().add(executeButton, BorderLayout.CENTER);
 
-        JPanel infoPanel = new JPanel(new GridLayout(2,2));
+        JPanel infoPanel = new JPanel(new GridLayout(2, 2));
         infoPanel.add(studentEdgar);
         infoPanel.add(studentAngel);
         infoPanel.add(courseName);
         infoPanel.add(teacherName);
-        infoPanel.setPreferredSize(new Dimension(600,100));
+        infoPanel.setPreferredSize(new Dimension(600, 100));
         getContentPane().add(infoPanel, BorderLayout.SOUTH);
 
         setVisible(true);
